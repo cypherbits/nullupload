4d6c5f4e32997c068b5466423fb0971e49043a73
juanjo <juanjo@avanix.es>
2020-03-01 12:52:26 +0100

    delete cluster project, won't do

--- cluster/public/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$_config_access_password = "eec2dd9b8c25a89ef43e4128c59eb904a38b7a3a28e571219b2ffbab82b9fcde"; //sha256()
-$_config_access_download_referer = "https://nullupload.com/download";
